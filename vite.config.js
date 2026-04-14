ea8d86dc275b0291618bf42008288f2681317a9a
aziz4567 <azizaloui987@gmail.com>
2026-04-14 23:34:09 +0100

    chore: update base path and repo name

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/alouimedaziz.github.io/', // <--- MAKE SURE THIS HAS SLASHES ON BOTH SIDES
+  base: '/', // <--- MAKE SURE THIS HAS SLASHES ON BOTH SIDES
 })
